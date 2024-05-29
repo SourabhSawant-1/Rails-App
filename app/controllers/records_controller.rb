a6bb5d54b6d909123f78dad06ff786b1f0ead682
Sourabh Sawant <sourabh.sawant@v2solutions.com>
2024-05-29 10:23:28 +0530

    Write a comment

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,7 +1,7 @@
 class RecordsController < ApplicationController
 
     def index
-        @records = Record.all
+        @records = Record.all  # This will display all the records inside the file.
     end
 
 end 
